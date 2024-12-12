d5074b754e7969df2eab7ad904f9ad64e26586a9
Dineth Dilshan Gunawardana <infodinethdil@gmail.com>
2024-12-12 11:31:09 +0530

    #1

---
+++ public/app/nav.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("nav-menu");
+
+    hamburger.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+});
